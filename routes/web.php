d4569b55c313dd88dced88ae2533673a5d205f77
Alvianto Hery Sarborn <alfiantoherysarborn@gmail.com>
2025-11-28 23:20:49 +0700

    Comment profile controller in route web.php

--- routes/web.php
+++ routes/web.php
@@ -82,11 +82,11 @@ Route::middleware(['auth', 'role:student'])
 | Profile Routes (Semua User)
 |--------------------------------------------------------------------------
 */
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
 
 /*
 |--------------------------------------------------------------------------
